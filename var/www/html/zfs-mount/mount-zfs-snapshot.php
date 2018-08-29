ceb54951950a654b5c643a2e42150f625babe250
John <john@spy-free.net>
2018-08-29 14:51:17 -0400

    added diff capability

--- var/www/html/zfs-mount/mount-zfs-snapshot.php
+++ var/www/html/zfs-mount/mount-zfs-snapshot.php
@@ -1,17 +1,37 @@
 <?php
 
-	$selected_snap = $_GET['selected_snapshot'];
-	$snapshot = explode(" ", $selected_snap);
+	$selected_snap1 = $_POST['selected_snapshot1'];
+	$selected_snap2 = $_POST['selected_snapshot2'];
+	$action = $_POST['action'];
 	
-	print("<html><body><pre><b>mount command is:</b> mount -t zfs $snapshot[0] /mnt/temp");
+	$snapshot1 = explode(" ", $selected_snap1);
+	$snapshot2 = explode(" ", $selected_snap2);
 	
-    exec("sudo /usr/bin/mount -t zfs $snapshot[0] /mnt/temp 2>&1", $results );
-    $lines = count($results);
+	print("<html><body><pre>");
 	
-	for ($line = 0; $line < $lines; $line +=1) {
-		print("$results[$line]<br>");
-    }
-	
-	print("<form method='GET' action='unmount-zfs-snapshot.php'></select><input type='submit' value='Unmount'></form></pre></body></html>");
+	if ($action == 'Mount') {
+
+		print("<b>mount command is:</b> mount -t zfs $snapshot1[0] /mnt/temp <br>");
+		
+		exec("sudo /usr/bin/mount -t zfs $snapshot1[0] /mnt/temp 2>&1", $results );
+		$lines = count($results);
+		
+		for ($line = 0; $line < $lines; $line +=1) {
+			print("$results[$line]<br>");
+		}
+		
+		print("<form method='GET' action='unmount-zfs-snapshot.php'></select><input type='submit' value='Unmount'></form></pre></body></html>");
+		
+	} else if ($action == 'Diff') {
+		
+		print("<b>diff command is:</b> zfs diff $snapshot1[0] $snapshot2[0] <br>");
+		
+		exec("sudo /usr/sbin/zfs diff $snapshot1[0] $snapshot2[0] 2>&1", $results );
+		$lines = count($results);
+		
+		for ($line = 0; $line < $lines; $line +=1) {
+			print("$results[$line]<br>");
+		}
+	} 
 	
 ?>
--- var/www/html/zfs-mount/zfs.php
+++ var/www/html/zfs-mount/zfs.php
@@ -4,12 +4,24 @@
 	$lines = count($results);
 	
 	
-    print("<html><body><pre><h3>Mount a ZFS snapshot</h3><br><b>Name                        Used Avail Refer MountPoint</b><br><form method='GET' action='mount-zfs-snapshot.php'><select name='selected_snapshot'>");
+    print("<html><body><pre><h3>Mount or Diff ZFS snapshot</h3><br><b>               Name                        Used Avail Refer MountPoint</b><br>");
+	
+	print("<form method='POST' action='mount-zfs-snapshot.php'><b>Earliest snap: </b><select name='selected_snapshot1'><option value=''></option>");
+	print("<option value = ''></option>");
 	
 	for ($line = 1; $line < $lines; $line +=1) {
 		$snap = explode(' ', $results[$line]);
 		print("<option value='$snap[0]'>$results[$line]</option>");
     }
-    print("</select><input type='submit' value='Mount'></form></pre></body></html>");
 	
+	print("</select><input type='submit' value='Mount' name='action'><br><b>Latest snap:   </b><select name='selected_snapshot2'><option value=''></option>");
+
+	for ($line = 1; $line < $lines; $line +=1) {
+		$snap = explode(' ', $results[$line]);
+		print("<option value='$snap[0]'>$results[$line]</option>");
+    }
+	
+	print("</select><input type='submit' value='Diff' name='action'></form>");
+	
+	print("</pre></body></html>");
 ?>
